14038c8a336231beda1396d8a2d71ce2cc287b9c
IMAC <tysim190@gmail.com>
2023-02-15 11:38:41 +0300

    Добавьте файлы проекта.

---
+++ CoffeeHouse/Windows/AthorizationWindow1.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CoffeeHouse.ClassHelper;
+using static CoffeeHouse.ClassHelper.EFClass;
+
+namespace CoffeeHouse.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AthorizationWindow1.xaml
+    /// </summary>
+    public partial class AthorizationWindow1 : Window
+    {
+        public AthorizationWindow1()
+        {
+            InitializeComponent();
+        }
+
+        private void TbRegist_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            RegistrationWindow1 registrationWindow1 = new RegistrationWindow1();
+            registrationWindow1.Show();
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var authUser = context.Authorization.ToList()
+            .Where(i => i.Login==LoginBox1.Text && i.Password== PassBx1.Password)
+            .FirstOrDefault();
+            if (authUser != null)
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Нет такого пользователя");
+            }
+        }
+    }
+}
---
+++ CoffeeHouse/Windows/RegistrationWindow1.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CoffeeHouse.ClassHelper;
+using static CoffeeHouse.ClassHelper.EFClass;
+
+namespace CoffeeHouse.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationWindow1.xaml
+    /// </summary>
+    public partial class RegistrationWindow1 : Window
+    {
+        public RegistrationWindow1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(TbFirst.Text))
+            {
+                MessageBox.Show("Фамилия не может быть пустой");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(TbLast.Text))
+            {
+                MessageBox.Show("Имя не может быть пустым");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(TbPatr.Text))
+            {
+                MessageBox.Show("Отчество не может быть пустым");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(TbNumber.Text))
+            {
+                MessageBox.Show("Номер телефона не может быть путсым");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(TbEmail.Text))
+            {
+                MessageBox.Show("Поле с почтой не может быть пустым");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(TbLog.Text))
+            {
+                MessageBox.Show("Логин не может быть путсым");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(TbPass.Password))
+            {
+                MessageBox.Show("Пароль не может быть путсым");
+                return;
+            }
+
+            var authUser = context.Authorization.ToList()
+            .Where(i => i.Login == TbLog.Text)
+            .FirstOrDefault();
+            if (authUser != null)
+            {
+
+                MessageBox.Show("Логин занят");
+                return ;
+            }
+            DB.Authorization acc = new DB.Authorization();
+            acc.LastName = TbLast.Text;
+            acc.FirstName = TbFirst.Text;
+            acc.Patronymic = TbPatr.Text;
+            acc.NumberPhone = TbNumber.Text;
+            acc.Email = TbEmail.Text;
+            acc.Login = TbLog.Text;
+            acc.Password = TbPass.Password;
+            context.Authorization.Add(acc);
+            context.SaveChanges();
+            MessageBox.Show("Ок");
+
+        }
+    }
+}
